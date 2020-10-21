ed80547f86bd33d1f658ef0c1454d5bc50eec144
mehreganmohseni <momohammadi@ce.sharif.edu>
2020-10-21 16:33:18 +0330

    Main is added

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,54 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        String s = "13.3e-3";
-        System.out.println(Double.parseDouble(s));
+    public static void main(String[] args) throws IOException {
+        try {
+            String inputFileName = null;
+            String outputFileName = null;
+            if (args != null) {
+                for (int i = 0; i < args.length; i++) {
+                    if (args[i].equals("-i")) {
+                        inputFileName = args[i + 1];
+                    }
+                    if (args[i].equals("-o")) {
+                        outputFileName = args[i + 1];
+                    }
+                }
+            }
+            Reader read = null;
+            if (inputFileName != null) {
+                read = new FileReader("../tests/" + inputFileName);
+            }
+            if (outputFileName != null) {
+                writer = new FileWriter("../out/" + outputFileName);
+            } else {
+                writer = new OutputStreamWriter(System.out);
+            }
+            main scanner = new main(read);
+            /*while ( !scanner.zzAtEOF ) */
+            scanner.yylex();
+            writer.flush();
+            writer.close();
+        } catch (Exception e) {
+            Writer writer;
+            String outputFile = null;
+            if (args != null) {
+                for (int i = 0; i < args.length; i++) {
+                    if (args[i].equals("-o")) {
+                        outputFile = args[i + 1];
+                    }
+                }
+            }
+            if (outputFile != null) {
+                writer = new FileWriter("out/" + outputFile);
+            } else {
+                writer = new OutputStreamWriter(System.out);
+            }
+
+            writer.write("NO");
+            writer.flush();
+            writer.close();
+        }
     }
 
 }
