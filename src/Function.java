e98b29428c363c59df475d5b44f50bc027751062
mehregan mohseni <mehreganmohseni@gmail.com>
2021-02-08 00:14:46 +0330

    functionn

--- src/Function.java
+++ src/Function.java
@@ -1,6 +1,24 @@
 import java.util.ArrayList;
 
 public class Function {
+
     private String name;
-    //private ArrayList<> parameter = new ArrayList<>();
+    private ArrayList<Variable> parameter = new ArrayList<Variable>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Variable> getParameter() {
+        return parameter;
+    }
+
+    public void setParameter(ArrayList<Variable> parameter) {
+        this.parameter = parameter;
+    }
+
 }
