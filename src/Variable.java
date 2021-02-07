dde58310997fecbac0506a60baca1e92dba40490
alirezaisavand <isavand.alireza@gmail.com>
2021-02-07 23:56:28 +0330

    Variable.java added

---
+++ src/Variable.java
@@ -0,0 +1,29 @@
+public class Variable {
+    private String type;
+    private String name;
+    private int number;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
