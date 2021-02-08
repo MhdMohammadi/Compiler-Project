a3496c62522c4de33418f08ec87a92dbce3391ae
mehregan mohseni <mehreganmohseni@gmail.com>
2021-02-08 21:40:22 +0330

    findVariable

--- src/Compiler.java
+++ src/Compiler.java
@@ -15,6 +15,25 @@ public class Compiler {
         }
     }
 
+    public Variable findVariable(Node node){
+        Node node1 = node;
+        while (true){
+            for (Variable variable: node1.getDefinedVariables()){
+                if (variable.getName().equals((String)node.getValue())){
+                    if (variable.getNumber() < node.getIndex()) {
+                        return variable;
+                    }
+                }
+            }
+            if (node1.getParent() == null){
+                break;
+            } else {
+                node1 = node1.getParent();
+            }
+        }
+        return null;
+    }
+
     public Node getRoot() {
         return root;
     }
