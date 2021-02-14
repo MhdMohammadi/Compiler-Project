d364e3f0091c1e43f46e4a11b04b317994719df3
Mohammad <m.mohammadi1378@outlook.com>
2021-02-14 22:25:38 +0330

    add something to codegenerator

--- src/CodeGenerator.java
+++ src/CodeGenerator.java
@@ -1,5 +1,5 @@
 public class CodeGenerator {
-    public Code readLine(){
+    public Code readLine() {
         Code code = new Code();
         code.addCode("li $v0, 5");
         code.addCode("syscall");
@@ -7,7 +7,7 @@ public class CodeGenerator {
         return code;
     }
 
-    public Code readInteger(){
+    public Code readInteger() {
         Code code = new Code();
         code.addCode("li $v0, 8");
         code.addCode("la $a0, buffer");
@@ -16,4 +16,27 @@ public class CodeGenerator {
         return code;
     }
 
+    public Code addExpr(Node node1, Node node2) {
+        Type t1 = node1.getType();
+        if (Type.getTypeByName("int", 0).equals(t1)) {
+            return addExprInt(node1, node2);
+        } else if (Type.getTypeByName("boolean", 0).equals(t1)) {
+            return addExprBoolean(node1, node2);
+        } else if (Type.getTypeByName("double", 0).equals(t1)){
+            return addExprDouble(node1, node2);
+        }
+        return null;
+    }
+
+    public Code addExprInt(Node node1, Node nod2){
+        return null;
+    }
+
+    public Code addExprBoolean(Node node1, Node node2){
+        return null;
+    }
+
+    public Code addExprDouble(Node node1, Node node2){
+        return null;
+    }
 }
