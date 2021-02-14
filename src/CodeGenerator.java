5185eadd8d4f0fd70562f563dd573fc20cd4475c
Mohammad <m.mohammadi1378@outlook.com>
2021-02-14 23:42:14 +0330

    add kardam ye chizaei

--- src/CodeGenerator.java
+++ src/CodeGenerator.java
@@ -26,19 +26,19 @@ public class CodeGenerator {
         }
     }
 
-    public Code addExpr(Node node1, Node node2) {
+    public Code calcExpr(Node node1, Node node2, Operator operator) {
         Type t1 = node1.getType();
         if (Type.getTypeByName("int", 0).equals(t1)) {
-            return addExprInt(node1, node2);
+            return calcIntExpr(node1, node2, operator);
         } else if (Type.getTypeByName("boolean", 0).equals(t1)) {
-            return addExprBoolean(node1, node2);
+            return calcBooleanExpr(node1, node2, operator);
         } else if (Type.getTypeByName("double", 0).equals(t1)){
-            return addExprDouble(node1, node2);
+            return calcDoubleExpr(node1, node2, operator);
         }
         return null;
     }
 
-    public Code addExprInt(Node node1, Node node2){
+    public Code calcIntExpr (Node node1, Node node2, Operator operator){
         Code code = new Code();
         code.addCode(node1.getCode());
         code.addCode("sub $sp, $sp, 4");
@@ -46,11 +46,27 @@ public class CodeGenerator {
         code.addCode(node2.getCode());
         code.addCode("lw $t1, 0($sp)");
         code.addCode("add $sp, $sp, 4");
-        code.addCode("add $t0, $t0, $t1");
+        switch (operator){
+            case PLUS:
+                code.addCode("add $t0, $t1, $t0");
+                break;
+            case MINUS:
+                code.addCode("sub $t0, $t1, $t0");
+                break;
+            case DIV:
+                code.addCode("div $t0, $t1, $t0");
+                break;
+            case MULT:
+                code.addCode("mul $t0, $t1, $t0");
+                break;
+            case MOD:
+                code.addCode("div $t0, $t1, $t0");
+                break;
+        }
         return code;
     }
 
-    public Code addExprBoolean(Node node1, Node node2){
+    public Code calcBooleanExpr(Node node1, Node node2, Operator operator){
         Code code = new Code();
         code.addCode(node1.getCode());
         code.addCode("sub $sp, $sp, 4");
@@ -58,19 +74,27 @@ public class CodeGenerator {
         code.addCode(node2.getCode());
         code.addCode("lw $t1, 0($sp)");
         code.addCode("add $sp, $sp, 4");
-        code.addCode("or $t0, $t0, $t1");
+        switch (operator){
+            case OROR:
+                code.addCode("or $t0, $t0, $t1");
+                break;
+            case ANDAND:
+                code.addCode("and $t0, $t0, $t1");
+                break;
+        }
         return code;
     }
+    //todo nemidunam ke or bayad bokonim ya na
 
-    public Code addExprDouble(Node node1, Node node2){
+    public Code calcDoubleExpr(Node node1, Node node2, Operator operator){
         Code code = new Code();
         code.addCode(node1.getCode());
         code.addCode("sub $sp, $sp, 4");
-        code.addCode("sw $f0, 0($sp)");
+        code.addCode("s.s $f0, 0($sp)");
         code.addCode(node2.getCode());
-        code.addCode("lw $f1, 0($sp)");
+        code.addCode("l.s $f1, 0($sp)");
         code.addCode("add $sp, $sp, 4");
-        code.addCode("add $f0, $f0, $f1");
+        code.addCode("add.s $f0, $f0, $f1");
         return code;
     }
 }
