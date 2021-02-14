40d63eaf295ab89515a6c76b3f080c484cb77746
Mohammad <m.mohammadi1378@outlook.com>
2021-02-14 22:03:58 +0330

    edit code generator

--- src/CodeGenerator.java
+++ src/CodeGenerator.java
@@ -1,15 +1,19 @@
 public class CodeGenerator {
-    public String readLine(){
-        String s = "";
-        s += "li $v0, 5\n";
-        System.out.println("syscall");
-        System.out.println("move $t0, $v0");
+    public Code readLine(){
+        Code code = new Code();
+        code.addCode("li $v0, 5");
+        code.addCode("syscall");
+        code.addCode("move $t0, $v0");
+        return code;
     }
 
-    public void readInteger(){
-        System.out.println("li $v0, 8");
-        System.out.println("la $a0, buffer");
-        System.out.println("li $a1, 1000");
-        System.out.println("syscall");
+    public Code readInteger(){
+        Code code = new Code();
+        code.addCode("li $v0, 8");
+        code.addCode("la $a0, buffer");
+        code.addCode("li $a1, 1000");
+        code.addCode("syscall");
+        return code;
     }
+
 }
