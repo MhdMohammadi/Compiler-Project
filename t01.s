cf315c00b2593572be8ef597bf7bb0b964def991
mehregan mohseni <mehreganmohseni@gmail.com>
2021-02-18 02:53:31 +0330

    continue

--- t01.s
+++ t01.s
@@ -15,7 +15,7 @@ li $t0, 0
 lw $t1, 0($sp)
 add $sp, $sp, 4
 sw $t0, 0($t1)
-L5:
+L1:
 sub $t0, $fp, 12
 lw $t0, 0($t0)
 sub $sp, $sp, 4
@@ -24,25 +24,31 @@ li $t0, 10
 lw $t1, 0($sp)
 add $sp, $sp, 4
 slt $t0, $t1, $t0
-beq $t0, 0, L6
+beq $t0, 0, L2
 sub $sp, $sp, 0
+sub $t0, $fp, 12
+lw $t0, 0($t0)
+li $v0, 1
+move $a0, $t0
+syscall
+la $a0, ENDL
+li $v0, 4
+syscall
 sub $t0, $fp, 12
 lw $t0, 0($t0)
 sub $sp, $sp, 4
 sw $t0, 0($sp)
-li $t0, 6
+li $t0, 7
 lw $t1, 0($sp)
 add $sp, $sp, 4
 seq $t0, $t1, $t0
-beq $t0, 0, L9
+beq $t0, 0, L4
 sub $sp, $sp, 0
-j L7
+j L2
 add $sp, $sp, 0
-j L8
-L9:
-L8:
-add $sp, $sp, 0
-L7:
+j L3
+L4:
+L3:
 sub $t0, $fp, 12
 sub $sp, $sp, 4
 sw $t0, 0($sp)
@@ -57,41 +63,9 @@ add $t0, $t1, $t0
 lw $t1, 0($sp)
 add $sp, $sp, 4
 sw $t0, 0($t1)
-j L5
-L6:
+add $sp, $sp, 0
+j L1
+L2:
 add $sp, $sp, 4
 li $v0, 10
 syscall
-L0 :
-lw $t0, 4($sp)
-beq $t0, 0, L10
-li $t0, 1
-j L11
-L10:
-li $t0, 0
-L11:
-move $v0, $t0
-lw  $ra, 8($fp)
-lw  $fp, 12($fp)
-jr $ra
-L1 :
-lw $t0, 4($fp)
-move $v0, $t0
-lw  $ra, 8($fp)
-lw  $fp, 12($fp)
-jr $ra
-L2 :
-l.s $f0, 4($fp)
-cvt.w.s $f0, $f0
-mfc1 $t0, $f0
-move $v0, $t0
-lw  $ra, 8($fp)
-lw  $fp, 12($fp)
-jr $ra
-L3 :
-lw $t0, 4($fp)
-mtc1 $t0, $f0
-cvt.s.w $f0, $f0
-lw  $ra, 8($fp)
-lw  $fp, 12($fp)
-jr $ra
