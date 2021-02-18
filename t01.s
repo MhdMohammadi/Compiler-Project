9e3654c55304569c7aeb1b65a4b3751c203881e5
Mohammad <m.mohammadi1378@outlook.com>
2021-02-18 12:25:43 +0330

    mikham pull konam

--- t01.s
+++ t01.s
@@ -11,35 +11,31 @@ sub $sp, $sp, 4
 sub $t0, $fp, 12
 sub $sp, $sp, 4
 sw $t0, 0($sp)
-li.s $f0, 3.2
+li $a0, 0
+li $v0, 9
+syscall
+move $t0, $v0
 lw $t1, 0($sp)
 add $sp, $sp, 4
-s.s $f0, 0($t1)
+sw $t0, 0($t1)
 sub $t0, $fp, 12
+lw $t0, 0($t0)
 sub $sp, $sp, 4
 sw $t0, 0($sp)
-lw $t0, 0($fp)
+li $t0, 2
 sub $sp, $sp, 4
 sw $t0, 0($sp)
-sub $t0, $fp, 12
-l.s $f0, 0($t0)
-sub $sp, $sp, 4
-s.s $f0, 0($sp)
 sub $sp, $sp, 8
 sw $fp, 4($sp)
 sw $ra, 0($sp)
 add $fp, $sp, 12
 jal L0
+move $t0, $v0
 lw $ra, 0($sp)
 lw $fp, 4($sp)
 add $sp, $sp, 16
-lw $t1, 0($sp)
-add $sp, $sp, 4
-s.s $f0, 0($t1)
-sub $t0, $fp, 12
-l.s $f0, 0($t0)
-li $v0, 2
-mov.s $f12, $f0
+li $v0, 1
+move $a0, $t0
 syscall
 la $a0, ENDL
 li $v0, 4
@@ -47,12 +43,3 @@ syscall
 add $sp, $sp, 4
 li $v0, 10
 syscall
-L0 :
-sub $sp, $sp, 0
-sub $t0, $fp, 4
-l.s $f0, 0($t0)
-sub $sp, $fp, 12
-jr $ra
-add $sp, $sp, 0
-sub $sp, $fp, 3
-jr $ra
