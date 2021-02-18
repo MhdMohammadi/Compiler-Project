d2f524a90eae46fa6e7e35b7ef7f2669af01eac7
alirezaisavand <isavand.alireza@gmail.com>
2021-02-18 15:50:54 +0330

    for pull

--- t01.s
+++ t01.s
@@ -11,27 +11,14 @@ sub $sp, $sp, 4
 sub $t0, $fp, 12
 sub $sp, $sp, 4
 sw $t0, 0($sp)
-li $v0, 5
-syscall
-move $t0, $v0
+li $t0, 0
 lw $t1, 0($sp)
 add $sp, $sp, 4
 sw $t0, 0($t1)
 sub $t0, $fp, 12
 lw $t0, 0($t0)
-beq $t0, 0, L1
-li $t0, 1
-j L2
-L1:
-li $t0, 0
-L2:
-beq $t0, 0, L3
-la $a0, TRUE
-j L4
-L3 :
-la $a0, FALSE
-L4 :
-li $v0, 4
+li $v0, 1
+move $a0, $t0
 syscall
 la $a0, ENDL
 li $v0, 4
